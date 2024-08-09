598a7be4a8db854b85f31c0a1376d262c6944841
DanValnicek <danvalnicek@gmail.com>
2024-08-09 10:23:46 +0200

    Finding homography works very well

--- HomographyProcessor.py
+++ HomographyProcessor.py
@@ -8,24 +8,16 @@ import numpy
 # not singular or close to it (determinant > 0)
 
 class HomographyProcessor:
-    sum_of_frame_homographies: numpy.matrix
-    n_of_summed_homographies: int
+
     current_frame_best_homography: numpy.matrix
     current_frame_homog_correspondences: int
-    best_score: float
 
     def __init__(self):
-        self.sum_of_frame_homographies = numpy.matrix('0 0 0; 0 0 0; 0 0 0')
-        self.n_of_summed_homographies = 0
         self.current_frame_best_homography = []
         self.current_frame_homog_correspondences = 0
-        self.best_score = 0
 
     def get_homography_matrix(self):
         return self.current_frame_best_homography
-        # if self.n_of_summed_homographies == 0:
-        #     return self.sum_of_frame_homographies
-        # return self.sum_of_frame_homographies / self.n_of_summed_homographies
 
     @staticmethod
     def check_array_shape(arr):
@@ -37,28 +29,21 @@ class HomographyProcessor:
             return True
         return False
 
-    def process(self, matches: numpy.ndarray, scores: numpy.ndarray):
+    def process(self, matches: numpy.ndarray):
         if not self.check_array_shape(matches[:, :2]):
             return
-        if matches.shape[0] < 4:
+        if self.current_frame_homog_correspondences > matches.shape[0]:
             return
-        best_indices = numpy.argsort(scores)[-4:]
-        score = sum(scores[best_indices])
-        if score < self.best_score:
+        homog, _ = cv2.findHomography(matches[:, 2:], matches[:, :2], cv2.RANSAC, 5.0)
+        if cv2.determinant(homog[:2, :2]) < 0:
             return
-        best_points = matches[best_indices]
-        homog, _ = cv2.findHomography(best_points[:, 2:], best_points[:, :2], cv2.RANSAC, 5.0)
-        if homog is None or cv2.determinant(homog[:2, :2]) < 0:
-            return
-        self.best_score = score
         self.current_frame_best_homography = homog
         self.current_frame_homog_correspondences = matches.shape[0]
 
-    def add_current_frame_and_start_new(self):
-        return
-        if self.current_frame_homog_correspondences == 0:
+    def homog_transform(self, width, height, frame):
+        if frame is None or width is None or height is None:
             return
-        self.sum_of_frame_homographies += self.current_frame_best_homography
-        self.n_of_summed_homographies = self.n_of_summed_homographies + 1
-        self.current_frame_best_homography = []
-        self.current_frame_homog_correspondences = 0
+        pts = numpy.float32([[0, 0], [0, height - 1], [width - 1, height - 1], [width - 1, 0]]).reshape(-1, 1, 2)
+        dst = cv2.perspectiveTransform(pts, self.get_homography_matrix())
+        perspective_mat = cv2.getPerspectiveTransform(numpy.float32(dst), pts)
+        return cv2.warpPerspective(frame, perspective_mat, (width, height))
--- videoImgExtraction.py
+++ videoImgExtraction.py
@@ -19,30 +19,17 @@ if __name__ == '__main__':
     with tempfile.TemporaryDirectory() as path:
         images_from_path = convert_from_path(args.pdf, output_file='pdf', fmt="jpg", output_folder=path,
                                              size=(720, None))
-
-        # directory, _, file = next(os.walk(path, topdown=True, onerror=None, followlinks=False))
-        # print(type(file[0]))
-        # print(directory)
-        # file = os.path.join(directory, file[4])
-        # file = os.path.join(path, sorted(os.listdir(path))[0])
         files = [os.path.join(path, file) for file in sorted(os.listdir(path))]
         print(len(files))
         h, w = numpy.shape(cv2.imread(files[0], cv2.IMREAD_GRAYSCALE))[:2]
-
         print("heigh: ", h, " width: ", w)
         g = GeoFormer(w, 0.8, no_match_upscale=False, ckpt='saved_ckpt/geoformer.ckpt', device='cuda')
-        # video = cv2.cudacodec.VideoCapture(args.video, apiPreference=cv2.CAP_FFMPEG)
         video = cv2.VideoCapture(args.video, apiPreference=cv2.CAP_FFMPEG)
+        video_duration = video.get(cv2.CAP_PROP_FRAME_COUNT) // video.get(cv2.CAP_PROP_FPS) * 1000
+        print(video_duration)
         print(video.get(cv2.CAP_PROP_FPS))
         print(video)
-        # img = './tmp/6b13a04a-73a9-41b5-8aef-a1d4dbdb1b78-01.tif'
-        # img = './tmp/70c37ebe-257f-4553-91c4-75e2411bddd5-01.jpg'
-        # img = './data/imgs/img.png'
-        # cnt = 15
-        cnt = 0
         pos = 0.0
-        # video.set(cv2.CAP_PROP_POS_MSEC, 27 * 60 * 1000 + 14000)
-        # video.set(cv2.CAP_PROP_POS_MSEC, 15000)
         while video.isOpened():
             newFrame, frame = video.read()
             print(video.get(cv2.CAP_PROP_POS_MSEC) / 1000)
@@ -51,31 +38,25 @@ if __name__ == '__main__':
                     if file is None:
                         continue
                     matches, kpts1, kpts2, scores = g.match_pairs(frame, file, is_draw=False)
-                    homo_checker.process(matches, scores)
-
+                    homo_checker.process(matches)
                     print('\r', matches.shape, file, end='')
-
-                homo_checker.add_current_frame_and_start_new()
                 print("")
             print(homo_checker.get_homography_matrix())
+            print("best correspondences: ", homo_checker.current_frame_homog_correspondences)
             print(pos)
-            pos = pos + 150 * 60 * 1000 / 20
-            if pos > 150 * 60 * 1000:
+            pos = pos + video_duration / 20
+            if pos > video_duration:
                 break
             print(video.set(cv2.CAP_PROP_POS_MSEC, pos))
         pos = 0
         for v in range(30):
-            # video.set(cv2.CAP_PROP_POS_AVI_RATIO, v * random.random() / 20)
-            pos = pos + (150 * 60 * 1000) / 30
-            if pos > 150 * 60 * 1000:
-                pass
-                # break
+            pos = pos + video_duration / 30
+            if pos > video_duration:
+                break
             print(video.set(cv2.CAP_PROP_POS_MSEC, pos))
-            pts = numpy.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-            _, frame = video.read()
-            dst = cv2.perspectiveTransform(pts, homo_checker.get_homography_matrix())
-            perspectiveM = cv2.getPerspectiveTransform(numpy.float32(dst), pts)
-            newFrame = cv2.warpPerspective(frame, perspectiveM, (w, h))
-            print("perspective ", perspectiveM)
+            got_img, frame = video.read()
+            if not got_img:
+                continue
+            newFrame = homo_checker.homog_transform(w, h, frame)
             cv2.imwrite(f"./data/imgs/{v}.png", newFrame)
         video.release()
