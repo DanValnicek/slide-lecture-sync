ac9457382defabef634546d264e6d7d77813620c
DanValnicek <danvalnicek@gmail.com>
2025-03-31 19:54:07 +0200

    added naem to environmetn crated PdfExtender.py
    Introduce a new PdfExtender module for future custom PDF operations. Update the environment file with a new project name and adjust channels for dependency management. Add a temporary debug print statement in the main PDF viewer script.

---
+++ src/PdfExtender.py
@@ -0,0 +1,10 @@
+from pypdf import PdfWriter
+
+CUSTOM_EXTENSION_NAME = '/DANV_SlideVideoSync'
+CUSTOM_INTERVALS_SUBKEY_NAME = "/SlideAppearanceIntervals"
+
+
+# class DANV_PdfExtender:
+
+    # @staticmethod
+    # def add_extension_info(pdf_writer: PdfWriter):
