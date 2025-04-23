239d7d0c8d8e53291f9b3191ab2b084dbfd8cef2
xvalni00 <xvalni00@stud.fit.vutbr.cz>
2025-04-23 09:52:59 +0200

    Working tf-idf testing

---
+++ src/HomographyProcessor.py
@@ -0,0 +1,144 @@
+import logging
+from datetime import timedelta
+
+import cv2
+import numpy
+import numpy as np
+from shapely.geometry import Polygon,box
+
+from src.Presentation import Presentation
+
+
+# good homography
+# > 10 correspondences
+# small reprojection error
+# not singular or close to it (determinant > 0)
+
+class HomographyProcessor:
+    current_frame_best_homography: numpy.matrix
+    current_frame_homog_correspondences: int
+
+    def __init__(self):
+        self.current_frame_best_homography = []
+        self.current_frame_homog_correspondences = 0
+
+    def get_homography_matrix(self):
+        return self.current_frame_best_homography
+
+    @staticmethod
+    def check_array_shape(arr):
+        # Check if the array is valid list of matches
+        if not isinstance(arr, numpy.ndarray):
+            return False
+        n, cols = arr.shape
+        if n >= 4 and (cols == 2 or cols == 6):
+            return True
+        return False
+
+    def process(self, matches: numpy.ndarray, kp1, kp2):
+        good = []
+        for m, n in matches:
+            if m.distance < 0.8 * n.distance:
+                good.append(m)
+        if len(good) > self.current_frame_homog_correspondences and len(good) > 200:
+            src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+            # if not self.check_array_shape(matches[:, :2]):
+            #     return
+            # if self.current_frame_homog_correspondences > matches.shape[0]:
+            #     return
+            # homog, _ = cv2.findHomography(matches[:, 2:], matches[:, :2], cv2.RANSAC, 5.0)
+            homog, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            mask = mask.ravel().tolist()
+            if cv2.determinant(homog[:2, :2]) < 0:
+                return None, None
+            self.current_frame_best_homography = homog
+            # self.current_frame_homog_correspondences = matches.shape[0]
+            self.current_frame_homog_correspondences = len(good)
+            return mask, good
+        return None, None
+
+    def homog_transform(self, width, height, frame):
+        if frame is None or width is None or height is None:
+            return
+        pts = numpy.float32([[0, 0], [0, height - 1], [width - 1, height - 1], [width - 1, 0]]).reshape(-1, 1, 2)
+        dst = cv2.perspectiveTransform(pts, self.get_homography_matrix())
+        perspective_mat = cv2.getPerspectiveTransform(numpy.float32(dst), pts)
+        perspective_mat = np.linalg.inv(perspective_mat)
+        return cv2.warpPerspective(frame, perspective_mat, (width, height))
+
+    def find_homography_in_video(self, video: cv2.VideoCapture, presentation: Presentation):
+        w, h = presentation.slides[0].get_size()
+        logging.getLogger(__name__).debug("heigh: " + str(h) + "width: " + str(w))
+        # g = GeoFormer(w, 0.8, no_match_upscale=False, device='cuda')
+        matcher = cv2.SIFT.create()
+        bfm = cv2.FlannBasedMatcher({"algorithm": 1, "trees": 5}, {"checks": 50})
+        video_duration = video.get(cv2.CAP_PROP_FRAME_COUNT) // video.get(cv2.CAP_PROP_FPS) * 1000
+        logging.getLogger(__name__).debug("video_duration: " + str(video_duration))
+        pos = 0.0
+        while video.isOpened():
+            newFrame, frame = video.read()
+            logging.getLogger(__name__).debug("video current second: " + str(video.get(cv2.CAP_PROP_POS_MSEC) / 1000))
+            if newFrame:
+                best_homog = self.get_homography_matrix()
+                print("")
+                for img in presentation.slides:
+                    good_homog = self.get_homography_matrix()
+                    kp2, desc2 = matcher.detectAndCompute(numpy.array(img), None)
+                    # matches, kpts1, kpts2, scores = g.match_pairs(frame, numpy.array(img), is_draw=False)
+                    kp1, desc1 = matcher.detectAndCompute(frame, None)
+                    matches = bfm.knnMatch(desc1, desc2, k=2)
+                    mask, goodMatches = self.process(matches, kp1, kp2)
+                    print('\r', len(matches), " slide n:", img.get_page_number(), end='')
+                    if type(self.get_homography_matrix()) is type(np.array) and self.get_homography_matrix().shape == (
+                            3, 3) and not numpy.array_equal(good_homog,
+                                                            self.get_homography_matrix()):
+                        dst = cv2.perspectiveTransform(
+                            numpy.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2),
+                            self.get_homography_matrix())
+                        tmpImg2 = cv2.polylines(np.array(img), [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+                        tmpImg = cv2.drawMatches(frame, kp1, tmpImg2, kp2, goodMatches, None,
+                                                 matchColor=(0, 255, 0), matchesMask=mask, singlePointColor=None,
+                                                 flags=2)
+                        cv2.imwrite(f"data/img{pos}.png", tmpImg)
+                        # print('\r', matches.shape, " slide n:", img.get_page_number(), end='')
+                        logging.getLogger(__name__).debug(logging.getLogger(__name__).getEffectiveLevel())
+                if (not numpy.array_equal(best_homog, self.get_homography_matrix()) and
+                        logging.getLogger(__name__).getEffectiveLevel() >= logging.DEBUG):
+                    cv2.imwrite(f"data/imgs/best_at_{timedelta(milliseconds=pos)}.png", frame)
+                    cv2.imwrite(f"data/imgs/best_at_{timedelta(milliseconds=pos)}_transformed.png",
+                                self.homog_transform(w, h, frame))
+
+                    logging.getLogger(__name__).debug(str(self.get_homography_matrix()))
+                    logging.getLogger(__name__).debug(
+                        "best correspondences: " + str(self.current_frame_homog_correspondences))
+            logging.getLogger(__name__).debug(str(pos))
+            pos = pos + video_duration / 20
+            if pos > video_duration:
+                break
+            video.set(cv2.CAP_PROP_POS_MSEC, pos)
+
+    @staticmethod
+    def reasonableHomography(homography, src_w, src_h, dst_w, dst_h) -> bool:
+        src_size = src_w * src_h
+        dst_size = dst_w * dst_h
+        min_scale_factor = dst_size / src_size
+        max_scale_factor = dst_size / (32 * 32)
+        sub_mat = np.linalg.det(homography[:2, :2])
+        if not min_scale_factor * 0.9 < sub_mat <= 1.1 * max_scale_factor:
+            return False
+        transformed_pts = cv2.perspectiveTransform(
+            numpy.float32([[0, 0], [0, src_h - 1], [src_w - 1, src_h - 1], [src_w - 1, 0]]).reshape(-1, 1, 2),
+            homography)
+        transformed_pts = [(int(x[0][0]), int(x[0][1])) for x in transformed_pts]
+        transformed_polygon = Polygon(transformed_pts)
+        shrinking_k = 0.8
+        untouchable_rect = box(
+            dst_w * (1 - shrinking_k),
+            dst_h * (1 - shrinking_k),
+            dst_w * shrinking_k,
+            dst_h * shrinking_k
+        )
+        if not untouchable_rect.within(transformed_polygon):
+            return False
+        return True
--- src/SlideMatcher.py
+++ src/SlideMatcher.py
@@ -1,13 +1,13 @@
 import sys
 from bisect import bisect
 from collections import defaultdict
-from math import sqrt
 from typing import Sequence
 import cv2
 import numpy as np
 from numpy import ndarray
-from shapely.geometry import Polygon, box
+
 from src import Presentation
+from src.HomographyProcessor import HomographyProcessor
 
 
 class SlideMatcher:
@@ -43,26 +43,24 @@ class SlideMatcher:
         homog2, mask2 = cv2.findHomography(dst_pts2, src_pts2, cv2.USAC_ACCURATE, 1.0)
         descriptors = [self.slideIdxToDescRange(slide_idx)[0] + matches[i][0].trainIdx for i, inlier in enumerate(mask2)
                        if inlier]
-        descriptors = list(dict.fromkeys(descriptors))
         if dbg_src_pts is not None:
             dbg_src_pts[:] = [(src_pts2[i], dst_pts2[i]) for i, inlier in enumerate(mask2) if inlier]
         return descriptors
 
     def pick_best_slide(self, matched_descriptors_from_all, slide_idxs):
         if slide_idxs == []:
-            return {}
+            return None
         if len(slide_idxs) == 1:
-            return {slide_idxs[0]: 1}
-        unique_descriptors = np.unique(np.vstack([self.descriptors[d] for d in matched_descriptors_from_all]), axis=0)
-        hist = {}
+            return slide_idxs[0]
+        unique_descriptors = np.unique(np.vstack(matched_descriptors_from_all), axis=0)
+        id_scores = []
         for slide_id in slide_idxs:
             matches = self.matcher.knnMatch(unique_descriptors, self.slideDescriptors(slide_id), k=1)
             distance = sum([1 if m[0].distance < 0.5 else 0 for m in matches])
-            score = distance / (sqrt(len(unique_descriptors)) * sqrt(self.slideDescCnt(slide_id)))
-            if score < 0.4:
-                continue
-            hist[slide_id] = score
-        return hist
+            id_scores.append((slide_id, distance))
+        max_match = max(id_scores, key=lambda x: x[1])
+        pruned_scores = [val for val in id_scores if val[1] > max_match[1] * 0.9]
+        return min(pruned_scores, key=lambda x: self.slideDescCnt(x[0]))[0]
 
     def find_all_similar_descriptors_indexes(self, desc_index):
         maxResults = 10
@@ -73,15 +71,13 @@ class SlideMatcher:
         similar_matches = self.flannIndex.radiusSearch(self.descriptors[desc_index].reshape(1, -1), radius=1,
                                                        maxResults=similar_matches[0])
         if desc_index not in similar_matches[1][0]:
-            return np.append(similar_matches[1][0], desc_index)
+            np.append(similar_matches[1][0], desc_index)
         return similar_matches[1][0]
 
     def detect_and_sort_descriptors_from_frame(self, frame, mask):
         kp, desc = self.sift_detector.detectAndCompute(frame, mask)
-        instance_cnt = defaultdict(list)
-        if desc is None:
-            return instance_cnt
         matches = self.flannIndex.knnSearch(desc, 2)
+        instance_cnt = defaultdict(list)
         for i, m in enumerate(zip(matches[0], matches[1])):
             desc_indices = m[0]
             desc_distance = m[1]
@@ -98,69 +94,64 @@ class SlideMatcher:
         return instance_cnt
 
     @staticmethod
-    def reasonableHomography(homography, src_w, src_h, dst_w, dst_h) -> bool:
-        src_size = src_w * src_h
-        dst_size = dst_w * dst_h
-        min_scale_factor = dst_size / src_size
-        max_scale_factor = dst_size / (32 * 32)
-        sub_mat = np.linalg.det(homography[:2, :2])
-        if not min_scale_factor * 0.9 < sub_mat <= 1.1 * max_scale_factor:
-            return False
-        transformed_pts = cv2.perspectiveTransform(
-            np.float32([[0, 0], [0, src_h - 1], [src_w - 1, src_h - 1], [src_w - 1, 0]]).reshape(-1, 1, 2),
-            homography)
-        transformed_pts = [(int(x[0][0]), int(x[0][1])) for x in transformed_pts]
-        transformed_polygon = Polygon(transformed_pts)
-        shrinking_k = 0.78
-        untouchable_rect = box(
-            dst_w * (1 - shrinking_k),
-            dst_h * (1 - shrinking_k),
-            dst_w * shrinking_k,
-            dst_h * shrinking_k
-        )
-        if not untouchable_rect.within(transformed_polygon):
-            return False
-        return True
-
-    @staticmethod
     def spatial_pruning_and_verification(src_dst_kps, frame_h_w, slide_h_w):
         src_pts = np.float32([v[0] for v in src_dst_kps]).reshape(-1, 1, 2)
         dst_pts = np.float32([v[1] for v in src_dst_kps]).reshape(-1, 1, 2)
         homog, mask = cv2.findHomography(dst_pts, src_pts, cv2.USAC_ACCURATE, 1.0)
         f_h, f_w, = frame_h_w
         s_h, s_w, = slide_h_w
-        if homog is None or not SlideMatcher.reasonableHomography(homog, f_w, f_h, s_w, s_h):
+        if homog is None or not HomographyProcessor.reasonableHomography(homog, f_w, f_h, s_w, s_h):
             return
         return homog
 
-    def matched_slide(self, frame, debug_info: list = None):
-        slides_keypoints = self.detect_and_sort_descriptors_from_frame(frame, None)
-        picked_descriptors = []
-        picked_slides = []
+    def matched_slide(self, frame, debug_info: list = None, mask=None):
+        slides_keypoints = self.detect_and_sort_descriptors_from_frame(frame, mask)
+        match_histogram = dict()
+        # picked_descriptors = []
+        # picked_slides = []
         homographies = dict()
         for slide_idx, src_dst_kps in slides_keypoints.items():
             if len(src_dst_kps) < 5:
                 continue
             homog = self.spatial_pruning_and_verification(
                 src_dst_kps, frame.shape[:2],
-                self.presentation.get_slide(slide_idx).image.size
-            )
+                                                          self.presentation.get_slide(slide_idx).image.size)
             if homog is None:
                 continue
             dbg_src_pts = []
             calc_result = self.warp_and_recompute_slide_descriptors(frame, homog, slide_idx, dbg_src_pts=dbg_src_pts)
-            if len(calc_result) < 10 or calc_result is None:
+            if calc_result is None:
                 continue
 
             homographies[slide_idx] = homog
-            picked_descriptors += calc_result
-            picked_slides.append(slide_idx)
+            # picked_descriptors.append(calc_result)
+            slide_desc_range = self.slideIdxToDescRange(slide_idx)
+            slide_desc_index = cv2.flann.Index(np.array([self.descriptors[d_idx] for d_idx in calc_result]),
+                                               {"algorithm": 1, "trees": 1})
+            # filtered_desc_count = len(picked_descriptors[0])
+
+            q_tf_idf_vec = np.zeros(slide_desc_range[1] - slide_desc_range[0], dtype=np.float32)
+            for d in calc_result:
+                same_q_cnt = slide_desc_index.radiusSearch(self.descriptors[d].reshape(1, -1), 0.5, 1)[0]
+                q_tf_idf_vec[d - slide_desc_range[0]] = (same_q_cnt / len(calc_result)) * self.dataset_idf[
+                    d - slide_desc_range[0]]
+
+            match_histogram[slide_idx] = np.dot(q_tf_idf_vec,
+                                                self.dataset_tf_idf[slide_desc_range[0]:slide_desc_range[1]]) / (
+                                                 np.linalg.norm(q_tf_idf_vec) * self.slide_tf_idf_norms[slide_idx])
+            # picked_slides.append(slide_idx)
             warped_img = cv2.warpPerspective(frame, homog, self.presentation.get_slide(slide_idx).image.size)
             if debug_info is not None and (debug_info is [] or len(debug_info) <= 3):
                 if len(debug_info) == 3:
                     debug_info.pop(-1)
                 best_keypoints1 = [cv2.KeyPoint(*x[0][0], 1) for x in dbg_src_pts]
                 best_keypoints2 = [cv2.KeyPoint(*x[1][0], 1) for x in dbg_src_pts]
+                # for i, m in enumerate(mask2):
+                #     if m:
+                #         best_keypoints2.append(cv2.KeyPoint(dst_pts2[i][0][0], dst_pts2[i][0][1], 1))
+                #         best_keypoints1.append(cv2.KeyPoint(src_pts2[i][0][0], src_pts2[i][0][1], 1))
+                # best_keypoints1 = [kp[m.queryIdx].pt for m in significant_kp]
+                # best_keypoints2 = [self.keypoints[m.trainIdx].pt for m in significant_kp]
                 debug_info.append({
                     'matched_slide': slide_idx,
                     'visual': cv2.drawMatches(
@@ -173,17 +164,21 @@ class SlideMatcher:
                         best_keypoints1,
                         [cv2.DMatch(i, i, 0) for i in range(len(best_keypoints1))],
                         None,
-                        matchColor=(0, 255, 0),
-                        singlePointColor=(255,0,0),
                         flags=cv2.DrawMatchesFlags_DEFAULT),
                     'homog': homog,
                     'warped_image': warped_img})
-                # debug_info = sorted(debug_info,
-                #                     key=lambda img_tup: match_histogram[img_tup['matched_slide']],
-                #                     reverse=True)
-        match_histogram = self.pick_best_slide(picked_descriptors, picked_slides)
+                debug_info = sorted(debug_info,
+                                    key=lambda img_tup: match_histogram[img_tup['matched_slide']],
+                                    reverse=True)
+        # best_slide = self.pick_best_slide(picked_descriptors, picked_slides)
+        # match_histogram[best_slide] = 1
+        # if best_slide is None and mask is not None:
+        #     return self.matched_slide(frame, debug_info)
         if match_histogram == {}:
             return match_histogram, None, None
+        # added_margin = 1.1
+        # full_mask = np.ones((np.array(self.presentation.slides[best_slide].image.size) * added_margin).astype(int))
+        # mask = cv2.warpPerspective(full_mask, np.linalg.inv(homographies[best_slide]), frame.shape[:2]).astype(np.uint8)
         return match_histogram, max(match_histogram, key=match_histogram.get), debug_info
 
     def descIdxToSlideIdx(self, train_id):
@@ -210,25 +205,23 @@ class SlideMatcher:
         self.keypoints = []
         for slide in self.presentation.get_all_slides():
             kp, desc = (self.sift_detector.detectAndCompute(np.array(slide.image), None))
+            self.descriptors.append(desc)
             self.keypoints += kp
             self.last_slide_kp_idx.append(len(self.keypoints))
-            if desc is None:
-                continue
-            self.descriptors.append(desc)
         np.set_printoptions(threshold=sys.maxsize)
         self.descriptors = np.vstack(self.descriptors)
         self.flannIndex = cv2.flann.Index(self.descriptors, {"algorithm": 1, "trees": 1})
         # calculate tf-idf
-        # descriptor_count = self.descriptors.shape[0]
-        # self.dataset_tf_idf = np.zeros(descriptor_count)
-        # slide_count = self.presentation.get_slide_cnt()
-        # for idx in range(0, slide_count):
-        #     n_in_frame = self.slideDescCnt(idx)
-        #     for i in range(*self.slideIdxToDescRange(idx)):
-        #         same_descriptors = self.find_all_similar_descriptors_indexes(i)
-        #         same_in_frame = sum(1 for x in same_descriptors if self.descIdxToSlideIdx(x) == idx)
-        #         df = len({self.descIdxToSlideIdx(x) for x in same_descriptors})
-        #         # td-idf weight from https://www.cs.toronto.edu/~fidler/slides/2022Winter/CSC420/lecture14.pdf#page=42
-        #         self.dataset_idf.append(np.log2(slide_count / df))
-        #         self.dataset_tf_idf[i] = (same_in_frame / n_in_frame) * self.dataset_idf[i]
-        #     self.slide_tf_idf_norms.append(np.linalg.norm(self.dataset_tf_idf[slice(*self.slideIdxToDescRange(idx))]))
+        descriptor_count = self.descriptors.shape[0]
+        self.dataset_tf_idf = np.zeros(descriptor_count)
+        slide_count = self.presentation.get_slide_cnt()
+        for idx in range(0, slide_count):
+            n_in_frame = self.slideDescCnt(idx)
+            for i in range(*self.slideIdxToDescRange(idx)):
+                same_descriptors = self.find_all_similar_descriptors_indexes(i)
+                same_in_frame = sum(1 for x in same_descriptors if self.descIdxToSlideIdx(x) == idx)
+                df = len({self.descIdxToSlideIdx(x) for x in same_descriptors})
+                # td-idf weight from https://www.cs.toronto.edu/~fidler/slides/2022Winter/CSC420/lecture14.pdf#page=42
+                self.dataset_idf.append(np.log2(slide_count / df))
+                self.dataset_tf_idf[i] = (same_in_frame / n_in_frame) * self.dataset_idf[i]
+            self.slide_tf_idf_norms.append(np.linalg.norm(self.dataset_tf_idf[slice(*self.slideIdxToDescRange(idx))]))
